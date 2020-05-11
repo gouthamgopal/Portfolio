ec5ae135bce094e6d0aaa6ffb478243670c51f81
Goutham Gopal <goutham.gopal94@gmail.com>
2020-05-11 02:47:35 -0400

    Updated contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+import "../styles/Contact.css";
 
 class Contact extends Component {
   render() {
-    return <div>Contact Me.</div>;
+    return (
+      <div className="contactComponent">
+        <h3>Want to Connect?</h3>
+        <div className="textOuter">
+          <p>
+            I am open to connecting with new people, my inbox is always open. I
+            am looking for new opportunities. Feel free to reach out to me, I
+            will get back to you as soon as I can!
+          </p>
+        </div>
+        <button type="button" className="btn btn-outline-success">
+          <a
+            href="mailto:goutham.gopal94@gmail.com"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            className="btn"
+          >
+            Let's Connect
+          </a>
+        </button>
+      </div>
+    );
   }
 }
 
